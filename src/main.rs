595e73f21c88b5452235fe60b4b31fecd6baeb72
Rubydragon <anton.reinhard@proton.me>
2024-12-28 12:53:35 +0100

    Compiles

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,51 @@ use std::net::TcpStream;
 use std::io::*;
 use std::fs::File;
 use std::fs;
-use gif::DecodeOptions;
 
 extern crate tinyppm;
 
 //const X_SIZE:i32 = 3840;
 //const Y_SIZE:i32 = 1080;
 
-struct RGB {
+#[derive(Clone)] struct RGB {
     r:u8,
     g:u8,
     b:u8,
 }
 
-fn get_image(filename: &String) {
+fn get_image(filename: &String) -> Vec<Vec<RGB>> {
     let ppm_image_result = tinyppm::ppm_loader::read_image_data(filename);
     let ppm_image = match ppm_image_result {
         Ok(image) => image,
         _ => panic!("unable to read specified image file!"),
     };
 
-    let mut frame: Vec<Vec<RGB>> = vec![vec![RGB(0, 0, 0); image.width()]; image.height()];
-    
-    for px in image.pixels(){
+    let mut frame: Vec<Vec<RGB>> = vec![vec![RGB{r: 0, g: 0, b: 0}; ppm_image.width()]; ppm_image.height()];
 
-}
-    // `ppm_image` is now a struct containing image with, height and pixels 
+    let mut x = 0;
+    let mut y = 0;
+
+    for px in ppm_image.pixels() {
+        frame[y][x] = RGB{
+            r: (*px >> 24u8) as u8,
+            g: (*px >> 16u8 & 0xFF) as u8,
+            b: (*px >> 8u8 & 0xFF) as u8,
+        };
+        x += 1;
+        if x >= ppm_image.width() {
+            y += 1;
+            x = 0;
+        }
+    }
+    
+    frame
 }
 
 fn pixel(
     stream:&mut TcpStream,
     x:u16,
     y:u16,
-    px::RGB,
+    px:RGB
 ) -> std::io::Result<()> {
     let send_str = format!("PX {} {} {:02x}{:02x}{:02x}\n", x, y, px.r, px.g, px.b);
     //println!("{}", send_str);
@@ -53,7 +65,6 @@ fn show_picture(
     println!("width: {} height: {}", width, height);
 
     let mut y = 0;
-    c += 1;
     for row in buf {
         let mut x = 0;
         for px in row {
@@ -62,37 +73,6 @@ fn show_picture(
         }
         y += 1;
     }
-    Ok(())
-}
-
-fn stream_gif(
-    stream:&mut TcpStream,
-    x_pos:u16,
-    y_pos:u16
-) -> std::io::Result<()> {
-    let mut decoder = DecodeOptions::new();
-    decoder.set_color_output(gif::ColorOutput::RGBA);
-    let mut decoder = decoder.read_info(GIF).unwrap();
-
-    let mut frames: Vec<Vec<u8>> = Vec::new();
-    let (mut width, mut height) = (0, 0);
-
-    loop {
-        let frame_info = match decoder.next_frame_info().unwrap() {
-            Some(info) => info,
-            None => break,
-        };
-
-        width = frame_info.width;
-        height = frame_info.height;
-
-        let mut frame: Vec<u8> =
-            vec![0; frame_info.width as usize * frame_info.height as usize * 4];
-
-        decoder.read_into_buffer(&mut frame).unwrap();
-        frames.push(frame);
-    }
-
     Ok(())
 }
 
@@ -105,10 +85,7 @@ fn main() -> std::io::Result<()> {
 
     let mut stream = TcpStream::connect("wall.c3pixelflut.de:1337")?;
 
-    stream_gif(
-        &mut stream,
-        3500,
-        0
-    );
+    let image = get_image(&String::from("./bee/bee_00001.ppm"));
+
     Ok(())
 } // the stream is closed here
